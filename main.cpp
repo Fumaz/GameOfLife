9e7ece69ab16a4c8de7272c4ac79359f2093e3a8
Fumaz <mail@fumaz.dev>
2020-11-15 18:04:59 -0500

    Added text and clear

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ const int NORMAL_FPS = 30;
 
 const int MIN = 0;
 
-const int CELLS_WIDTH = 125;
-const int CELLS_HEIGHT = 125;
+const int CELLS_WIDTH = 75;
+const int CELLS_HEIGHT = 75;
 
-const int RESOLUTION_WIDTH = 750;
-const int RESOLUTION_HEIGHT = 750;
+const int RESOLUTION_WIDTH = (750 - (750 % CELLS_WIDTH));
+const int RESOLUTION_HEIGHT = (750 - (750 % CELLS_HEIGHT));
 
 const int RATIO_WIDTH = RESOLUTION_WIDTH / CELLS_WIDTH;
 const int RATIO_HEIGHT = RESOLUTION_HEIGHT / CELLS_HEIGHT;
@@ -21,7 +21,10 @@ bool running = false;
 bool cells[CELLS_WIDTH][CELLS_HEIGHT];
 bool oldCells[CELLS_WIDTH][CELLS_HEIGHT];
 
-int ticks = 1;
+int ticks = 5;
+
+float overlaySeconds = -1;
+string overlayText;
 
 bool isInRange(int x, int y) {
     return x > MIN && y > MIN && x < CELLS_WIDTH && y < CELLS_HEIGHT;
@@ -91,13 +94,63 @@ void simulate() {
 void draw(Image *image) {
     for (int x = 0; x < CELLS_WIDTH; x++) {
         for (int y = 0; y < CELLS_HEIGHT; y++) {
-            image->setPixel(x, y, cells[x][y] ? Color::Blue : Color::White);
+            image->setPixel(x, y, cells[x][y] ? Color::Black : Color::White);
+        }
+    }
+}
+
+void clearCells() {
+    for (int x = 0; x < CELLS_WIDTH; x++) {
+        for (int y = 0; y < CELLS_HEIGHT; y++) {
+            cells[x][y] = false;
+        }
+    }
+}
+
+void randomCells() {
+    srand(time(nullptr));
+
+    // TODO
+}
+
+void displayText(RenderWindow *window, const string &text) {
+    Font font;
+
+    if (!font.loadFromFile("/usr/share/fonts/TTF/OpenSans-Regular.ttf")) {
+        exit(1337);
+    }
+
+    Text str(text, font, 100);
+    str.setFillColor(Color::Magenta);
+
+    sf::FloatRect textRect = str.getLocalBounds();
+    str.setOrigin(textRect.left + textRect.width / 2.0f,
+                  textRect.top + textRect.height / 2.0f);
+    str.setPosition(sf::Vector2f(window->getSize().x / 2.0f, window->getSize().y / 2.0f));
+
+    window->draw(str);
+}
+
+void setOverlay(Clock *clock, const string &text, float seconds) {
+    overlaySeconds = seconds;
+    overlayText = text;
+
+    clock->restart();
+}
+
+void handleOverlay(RenderWindow *window, Clock *clock) {
+    if (overlaySeconds >= 0) {
+        if (overlaySeconds > clock->getElapsedTime().asSeconds()) {
+            displayText(window, overlayText);
+        } else {
+            overlaySeconds = -1;
         }
     }
 }
 
 int main() {
-    RenderWindow window(sf::VideoMode(RESOLUTION_WIDTH, RESOLUTION_HEIGHT), "Conway's Game Of Life", Style::Titlebar | Style::Close);
+    RenderWindow window(sf::VideoMode(RESOLUTION_WIDTH, RESOLUTION_HEIGHT), "Conway's Game Of Life",
+                        Style::Titlebar | Style::Close);
     Texture texture;
     texture.create(CELLS_WIDTH, CELLS_HEIGHT);
     Image image;
@@ -105,6 +158,8 @@ int main() {
 
     Sprite sprite;
     sprite.setScale(RATIO_WIDTH, RATIO_HEIGHT);
+
+    Clock clock;
 
     while (window.isOpen()) {
         Event event;
@@ -117,16 +172,34 @@ int main() {
                     toggleCellAt(event.mouseButton.x, event.mouseButton.y);
                 }
             } else if (event.type == Event::KeyReleased) {
-                if (event.key.code == Keyboard::Key::Space) {
-                    running = !running;
+                switch (event.key.code) {
+                    case Keyboard::Key::Space:
+                        running = !running;
+                        updateTicks(&window);
+                        setOverlay(&clock, running ? "RUN" : "PAUSE", 1);
 
-                    updateTicks(&window);
-                } else if (event.key.code == Keyboard::Key::Left) {
-                    ticks--;
-                    updateTicks(&window);
-                } else if (event.key.code == Keyboard::Key::Right) {
-                    ticks++;
-                    updateTicks(&window);
+                        break;
+                    case Keyboard::Key::Left:
+                        ticks--;
+                        updateTicks(&window);
+                        setOverlay(&clock, "SPEED +", 1);
+
+                        break;
+                    case Keyboard::Key::Right:
+                        ticks++;
+                        updateTicks(&window);
+                        setOverlay(&clock, "SPEED -", 1);
+
+                        break;
+                    case Keyboard::Key::C:
+                        clearCells();
+                        setOverlay(&clock, "CLEAR", 1);
+
+                        break;
+                    case Keyboard::Key::R:
+                        randomCells();
+
+                        break;
                 }
             }
         }
@@ -143,6 +216,8 @@ int main() {
         sprite.setTexture(texture);
 
         window.draw(sprite);
+        handleOverlay(&window, &clock);
+
         window.display();
     }
 
